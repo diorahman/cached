d85d81d408fae9dee9d8710d80e83592e452bf78
Dhi Aurrahman <dio@hooq.tv>
2017-05-23 18:17:46 +0700

    Add connection singleton per config

--- storage.js
+++ storage.js
@@ -1,15 +1,26 @@
 const redis = require('redis')
+const hash = require('string-hash')
 const { promisifyAll } = require('bluebird')
 
 promisifyAll(redis.RedisClient.prototype)
 promisifyAll(redis.Multi.prototype)
 
+const connections = new Map()
+
 function create (config) {
-  return redis.createClient({
+  const connection = redis.createClient({
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD
   })
+
+  connections.set(hash(JSON.stringify(config)), connection)
+  return connection
 }
 
-module.exports = create
+function get (config) {
+  const key = hash(JSON.stringify(config))
+  return connections.get(key) || create(key)
+}
+
+module.exports = get
